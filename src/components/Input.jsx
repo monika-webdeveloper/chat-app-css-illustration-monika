ecff12d42de3ab3706064ff4b980ffeef1dbe5f3
pioggia73 <monikawasilewska13@wp.pl>
2026-03-31 09:07:59 +0200

    improvements1

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,11 +2,26 @@ import SendButton from "../images/SendButton.png";
 
 const Input = () => {
   return (
+    // <div className='bg-white text-gray-300 text-[8px] font-normal leading-[120%] tracking-normal rounded-2xl py-1 pl-4 pr-2 flex justify-between items-center'>
+    //   <div className='w-5'>
+    //     <img src={SendButton} alt='type button' className='' />
+    //   </div>
+    // </div>
+
     <div className='bg-white text-gray-300 text-[8px] font-normal leading-[120%] tracking-normal rounded-2xl py-1 pl-4 pr-2 flex justify-between items-center'>
-      Type a message...
-      <div className='w-5'>
-        <img src={SendButton} alt='type button' className='' />
-      </div>
+      <label htmlFor='message'>
+        <input
+          type='text'
+          placeholder='Type a message'
+          required
+          id='message'
+          name='message'
+          aria-label='Type a message'
+        />
+      </label>
+      <button className='w-5' type='button' aria-label='Send message'>
+        <img src={SendButton} alt='send button' />
+      </button>
     </div>
   );
 };
--- src/components/MessageOption.jsx
+++ src/components/MessageOption.jsx
@@ -1,14 +1,20 @@
 const MessageOption = ({ price, option }) => {
   return (
-    <div className='flex gradient p-2 items-center justify-between w-37.75 rounded-t-lg rounded-br-lg rounded-bl-sm'>
-      <div className='flex gap-3 items-center'>
-        <div className='rounded-full w-3 h-3 border-pink-400 border'></div>
-        <p className='text-white text-[8px] font-normal leading-[120%] tracking-normal'>
+    <button
+      className='flex gradient p-2 items-center justify-between w-37.75 rounded-t-lg rounded-br-lg rounded-bl-sm'
+      type='button'
+    >
+      <span className='flex gap-3 items-center'>
+        <span
+          className='rounded-full w-3 h-3 border-pink-400 border'
+          aria-hidden='true'
+        ></span>
+        <span className='text-white text-[8px] font-normal leading-[120%] tracking-normal'>
           {option}
-        </p>
-      </div>
-      <p className='text-white text-xs'>{price}</p>
-    </div>
+        </span>
+      </span>
+      <span className='text-white text-xs'>{price}</span>
+    </button>
   );
 };
 export default MessageOption;
